2807f08a2c446093d27a36cc741ba30679de84e8
Harsh Gupta <harshgupta8512@gmail.com>
2025-11-02 03:37:06 +0530

    add vercel.json

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
-import {httpServer} from "./app.js"
+import {httpServer} from "./app.js" // This part is correct
+
 dotenv.config({
     path : "./.env"
 })
 
 
-
-
 connectDB()
-.then(httpServer.listen(process.env.PORT,()=>{
-    console.log("MONGO connected and server is running... ",process.env.PORT);
-    
-}))
-.catch((err)=> console.log("MONGO db connection failed !! ", err))
+.then(() => {
+    console.log("MONGO connected successfully.");
+})
+.catch((err) => {
+    console.error("MONGO db connection failed !! ", err);
+})
+
+
+export default httpServer;
