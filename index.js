ad5b8cce582f0d3d701facb8dc4e917ef56af9b5
Harsh Gupta <harshgupta8512@gmail.com>
2025-11-02 10:56:39 +0530

    fix vercel.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import connectDB from "./db/index.js"
+import connectDB from "./src/db/index.js"
 import dotenv from "dotenv"
-import {httpServer} from "./app.js"
+import {httpServer} from "./src/app.js"
 dotenv.config({
     path : "./.env"
 })
@@ -14,3 +14,5 @@ connectDB()
     
 }))
 .catch((err)=> console.log("MONGO db connection failed !! ", err))
+
+export default httpServer;
